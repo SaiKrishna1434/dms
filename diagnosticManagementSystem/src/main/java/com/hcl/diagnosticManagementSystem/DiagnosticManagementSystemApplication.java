9eb6ae75e2a2e6dd8e3f679473faad789d84bba7
SaiKrishna1434 <palakurisaikr.goud@hcltech.com>
2025-05-14 15:27:53 +0530

    Add files via upload

---
+++ diagnosticManagementSystem/src/main/java/com/hcl/diagnosticManagementSystem/DiagnosticManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.hcl.diagnosticManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiagnosticManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiagnosticManagementSystemApplication.class, args);
+		System.out.println("server started");
+	}
+
+}
---
+++ diagnosticManagementSystem/src/test/java/com/hcl/diagnosticManagementSystem/DiagnosticManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.diagnosticManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiagnosticManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

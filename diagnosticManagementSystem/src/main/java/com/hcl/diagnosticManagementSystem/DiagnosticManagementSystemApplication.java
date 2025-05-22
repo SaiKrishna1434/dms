2781bbe4db9c3602e52717ac8775fbe9f7bb99f0
Parvej Alam <parvej-alam@hcltech.com>
2025-05-22 13:51:15 +0530

    add healthcheckup

--- diagnosticManagementSystem/src/main/java/com/hcl/diagnosticManagementSystem/DiagnosticManagementSystemApplication.java
+++ diagnosticManagementSystem/src/main/java/com/hcl/diagnosticManagementSystem/DiagnosticManagementSystemApplication.java
@@ -1,6 +1,8 @@
 package com.hcl.diagnosticManagementSystem;
 
-import org.slf4j.Logger;
+
+import java.lang.*;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,11 +23,8 @@ public class DiagnosticManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiagnosticManagementSystemApplication.class, args);
-<<<<<<< HEAD
-		logger.info("server started successfully");
-=======
 		log.info("Application Started");
->>>>>>> 3db9b5a36d976dfa8e568db99248ac3875de5fa5
+
 	}
 }
 
--- diagnosticManagementSystem/src/main/java/com/hcl/diagnosticManagementSystem/controller/AppointmentController.java
+++ diagnosticManagementSystem/src/main/java/com/hcl/diagnosticManagementSystem/controller/AppointmentController.java
@@ -1,16 +1,18 @@
 package com.hcl.diagnosticManagementSystem.controller;
 
-import com.hcl.diagnosticManagementSystem.apiResponseModel.CustomResponseModel;
-import com.hcl.diagnosticManagementSystem.dto.AppointmentCheckupRequest;
-import com.hcl.diagnosticManagementSystem.dto.AppointmentCheckupResponse;
-import com.hcl.diagnosticManagementSystem.service.AppointmentService;
-import jakarta.validation.Valid;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.hcl.diagnosticManagementSystem.apiResponseModel.CustomResponseModel;
+import com.hcl.diagnosticManagementSystem.dto.AppointmentCheckupRequest;
+import com.hcl.diagnosticManagementSystem.dto.AppointmentCheckupResponse;
+import com.hcl.diagnosticManagementSystem.service.AppointmentService;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/appointment")

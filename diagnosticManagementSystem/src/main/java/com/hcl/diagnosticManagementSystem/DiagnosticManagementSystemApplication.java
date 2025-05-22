3db9b5a36d976dfa8e568db99248ac3875de5fa5
gopalpapewar23 <papewargopal.kishor@hcltech.com>
2025-05-22 11:51:39 +0530

    Use logger in my code

--- diagnosticManagementSystem/src/main/java/com/hcl/diagnosticManagementSystem/DiagnosticManagementSystemApplication.java
+++ diagnosticManagementSystem/src/main/java/com/hcl/diagnosticManagementSystem/DiagnosticManagementSystemApplication.java
@@ -1,14 +1,18 @@
 package com.hcl.diagnosticManagementSystem;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiagnosticManagementSystemApplication {
+	
+	private static final Logger log=LoggerFactory.getLogger(DiagnosticManagementSystemApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiagnosticManagementSystemApplication.class, args);
-		System.out.println("server started");
+		log.info("Application Started");
 	}
 }
 
